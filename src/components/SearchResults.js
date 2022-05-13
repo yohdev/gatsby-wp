0f0717f1619e6d1997cd3fa611ab2253b713d7e6
vkaukeano <von.kaukeano@temple.edu>
2022-05-13 16:30:20 -0400

    search working

---
+++ src/components/SearchResults.js
@@ -0,0 +1,98 @@
+
+   
+import React, { useEffect, useState } from "react";
+import lunr from "lunr";
+
+const SearchResults = props => {
+  const {
+    data: {
+      allWp: { edges: nodes }
+    },
+    onSelect
+  } = props;
+
+  const posts = nodes[0].node.searchResults;
+
+
+  // Default props
+  const minCharacters = props.minCharacters || 2;
+  const contentCharacters = props.contentCharacters || 200;
+  const maxResults = props.maxResults || 10;
+  const placeholder = props.placeholder || "Search...";
+
+  const [query, setQuery] = useState("");
+  const [idx, setIdx] = useState(null);
+
+  let results;
+  if (idx && query) {
+    results = idx.search(`*${query}*`).slice(0, maxResults);
+  }
+
+  useEffect(() => {
+    const idx = getIndex();
+    setIdx(idx);
+  });
+
+  const getIndex = () => {
+    return lunr(function() {
+      this.ref("id");
+      this.field("title");
+      this.field("content");
+      this.field("path");
+
+      posts.map(post => {
+        return this.add({
+          id: post.id,
+          title: post.post_title,
+          content: post.searchData,
+          path: post.pathname
+        });
+      });
+    });
+  };
+
+  return (
+    <div className={`container`}>
+      <input
+        name='query'
+        onChange={e => setQuery(e.target.value)}
+        autoComplete='off'
+        placeholder={placeholder}
+        className={`input`}
+      />
+
+      {query.length >= minCharacters && (
+        <ul className={`suggests`}>
+          {results &&
+            results.map(o => {
+              const post = posts.filter(post => post.id === o.ref)[0];
+
+              return (
+                <li key={post.id} className={`suggest`}>
+                  <h4
+                    className={`suggestTitle`}
+                    onClick={() => onSelect(post)}
+                    children={post.post_title}
+                  />
+
+                  {post.searchData && (
+                    <div
+                      className={`suggestContent`}
+                      dangerouslySetInnerHTML={{
+                        __html: post.searchData.substring(
+                          0,
+                          contentCharacters
+                        )
+                      }}
+                    />
+                  )}
+                </li>
+              );
+            })}
+        </ul>
+      )}
+    </div>
+  );
+};
+
+export default SearchResults;
