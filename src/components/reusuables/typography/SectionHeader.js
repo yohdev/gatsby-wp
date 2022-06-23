3ac74b99208763fe2e9e46004c65d3fed0ab430f
Travis Davis <skoolboi434@gmail.com>
2022-06-23 16:26:56 -0400

    Changed props to prop name

--- src/components/reusuables/typography/SectionHeader.js
+++ src/components/reusuables/typography/SectionHeader.js
@@ -1,7 +1,7 @@
 import React from "react"
 
-const SectionHeader = (props) => {
-  return <h3 className={`section-header ${props.color}`}>{props.heading}</h3>
+const SectionHeader = ({ heading, color }) => {
+  return <h3 className={`section-header ${color}`}>{heading}</h3>
 }
 
 export default SectionHeader
