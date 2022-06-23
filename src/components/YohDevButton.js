10038d32e2dfe3b1c2586df00b9ac07d25f94402
Travis Davis <skoolboi434@gmail.com>
2022-06-23 16:43:28 -0400

    changed props to prop name

--- src/components/YohDevButton.js
+++ src/components/YohDevButton.js
@@ -1,8 +1,8 @@
 import React from "react"
 import { Button } from "react-bootstrap"
 
-const YohDevButton = (props) => {
-  return <Button className={`${props.type}`}>{props.title}</Button>
+const YohDevButton = ({ type, title }) => {
+  return <Button className={`${type}`}>{title}</Button>
 }
 
 export default YohDevButton
