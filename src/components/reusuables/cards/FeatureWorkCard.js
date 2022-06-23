4cbdb0d1b868f9d9bcbe17c009359a24437d3ccc
Travis Davis <skoolboi434@gmail.com>
2022-06-23 16:42:19 -0400

    Changed props to prop name

--- src/components/reusuables/cards/FeatureWorkCard.js
+++ src/components/reusuables/cards/FeatureWorkCard.js
@@ -1,15 +1,15 @@
 import React from "react"
 import { StaticImage } from "gatsby-plugin-image"
 
-const FeatureWorkCard = (props) => {
+const FeatureWorkCard = ({ title, text }) => {
   return (
     <div className="carousel-card">
       <div className="card-img">
         <StaticImage src="../../../assets/images/kulhiImage.jpg" />
       </div>
       <div className="carousel-card-content">
-        <h3 className="title">{props.title}</h3>
-        <span className="type">{props.text}</span>
+        <h3 className="title">{title}</h3>
+        <span className="type">{text}</span>
       </div>
     </div>
   )
