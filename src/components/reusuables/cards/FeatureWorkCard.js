b6606e19487e776e45db0c41e34668efcb5e94f7
Travis Davis <skoolboi434@gmail.com>
2022-06-26 20:51:28 -0400

    Changed static image to dynamic

--- src/components/reusuables/cards/FeatureWorkCard.js
+++ src/components/reusuables/cards/FeatureWorkCard.js
@@ -1,13 +1,14 @@
 import React from "react"
-import { StaticImage } from "gatsby-plugin-image"
+import { GatsbyImage, getImage } from "gatsby-plugin-image"
 
-const FeatureWorkCard = ({ title, text }) => {
+const FeatureWorkCard = ({ title, text, image }) => {
+  const showImage = getImage(image)
   return (
-    <div className="carousel-card">
+    <div className="feat-work-card">
       <div className="card-img">
-        <StaticImage src="../../../assets/images/kulhiImage.jpg" />
+        <img src={image} />
       </div>
-      <div className="carousel-card-content">
+      <div className="feat-work-card-content">
         <h3 className="title">{title}</h3>
         <span className="type">{text}</span>
       </div>
