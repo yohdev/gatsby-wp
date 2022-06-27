acb4bf0fcb5e3eb6ef0f34de7ab726e94362f313
Travis Davis <skoolboi434@gmail.com>
2022-06-26 23:01:11 -0400

    Restructured the way the data was being used

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,27 +1,27 @@
 import React from "react"
-import { Container, Row, Col } from "react-bootstrap"
+import { Container, Row, Col, Image } from "react-bootstrap"
 import heroHome from "../assets/images/heroHome.jpg"
 import { StaticImage } from "gatsby-plugin-image"
 import YohDevButton from "./YohDevButton"
 
+const bgImage =
+  "http://www.travisdavisdeveloper.com/wp-content/uploads/2022/06/heroHero-scaled.jpg"
+
 const sectionStyle = {
-  backgroundImage: `url(${heroHome})`,
+  backgroundImage: `url(${bgImage})`,
 }
 
-const Hero = ({ heading, subtitle }) => {
+const Hero = (data) => {
   return (
     <section className="hero frontpage" style={sectionStyle}>
       <Container>
         <Row className="align-items-md-center">
           <Col lg={6} className="left">
-            <StaticImage
-              src={`../assets/images/yohdevHeroLeft.png`}
-              className="img-fluid"
-            />
+            <Image src={data.imgLeft} className="img-fluid" />
           </Col>
           <Col lg={6} className="right text-center">
-            <h3 className="heading">{heading}</h3>
-            <p className="subtitle">{subtitle}</p>
+            <h3 className="heading">{data.heading}</h3>
+            <p className="subtitle">{data.subtitle}</p>
             <YohDevButton type="gradient-btn" title="Contact Us" />
           </Col>
         </Row>
