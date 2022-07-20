1d5eddd0dce5ee3d080d7df833414f298ac612b8
Travis Davis <skoolboi434@gmail.com>
2022-07-20 12:10:03 -0400

    Case Study Hero built, fixed resuable spelling

---
+++ src/pages/case-studies.js
@@ -0,0 +1,17 @@
+import React from "react"
+import Hero from "../components/Hero"
+
+const caseStudies = () => {
+  return (
+    <div className="page-container">
+      <Hero
+        heading="Case Studies"
+        subtitle="We’re focused on delivering custom web solutions that drive action and help you thrive in even the most saturated industries."
+        btnType="gradient-btn"
+        btnTitle="Contact Us"
+      />
+    </div>
+  )
+}
+
+export default caseStudies
