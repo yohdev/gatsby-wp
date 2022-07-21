4f95bbc3f34a3de79ef858ca5e914228e36f3451
Travis Davis <skoolboi434@gmail.com>
2022-07-21 12:56:49 -0400

    Case Study Single page added, fixed spelling error

---
+++ src/pages/case-studies-single.js
@@ -0,0 +1,14 @@
+import React from "react"
+import Header from "../components/Header"
+import Footer from "../components/Footer"
+
+const caseStudiesSingle = () => {
+  return (
+    <div className="page-container">
+      <Header />
+      case-studies-single
+    </div>
+  )
+}
+
+export default caseStudiesSingle
